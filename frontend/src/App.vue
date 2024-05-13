6c5fb32a0ec98a354a59218ad3f258b28cec56ae
wnso.kim <wnso.kim@gmail.com>
2024-05-13 15:22:00 +0900

    🔨 Init: VueJs + Tailwind

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
