9415bf639d78f72987006bb4e381f7852cfe6a19
wnso-kim <wnso.kim@gmail.com>
2024-05-16 12:04:15 +0900

    ✨Feat: 구글 맵 load
    - @/components/Map.vue 의 api-key값 비워둠
    - .env에 설정 값 추가할 예정

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,8 +1,12 @@
 <script setup>
+import NavBar from '@/layout/NavBar.vue'
 import SearchBar from "@/components/SearchBar.vue"
 </script>
 
 <template>
+  <header>
+    <NavBar/>
+  </header>
   <div class="text-center mb-8"> <!-- 부모 요소에 text-center 클래스 추가 -->
     <img src="@/assets/img/logo-big-yeoro.png" alt="" class="mx-auto mb-8"> <!-- 이미지 감싸는 div에 mx-auto 클래스 추가 -->
     <h1>여행을 떠나고 싶으신가요? 우리 사이트좀 이용해주세요!!</h1>
---
+++ frontend/src/views/MapView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Map from "@/components/Map.vue"
+</script>
+
+<template>
+  <div class="text-center mb-8"> <!-- 부모 요소에 text-center 클래스 추가 -->
+    <Map />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
