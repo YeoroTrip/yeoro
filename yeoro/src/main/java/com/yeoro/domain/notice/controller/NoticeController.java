1f3fce926513335c1479e1daa16e2107b06d8727
wnso-kim <wnso.kim@gmail.com>
2024-05-17 19:15:59 +0900

    ✨Feat: 공지사항 API SWAGGER 예제 추가

--- yeoro/src/main/java/com/yeoro/domain/notice/controller/NoticeController.java
+++ yeoro/src/main/java/com/yeoro/domain/notice/controller/NoticeController.java
@@ -3,6 +3,10 @@ package com.yeoro.domain.notice.controller;
 import com.yeoro.domain.notice.model.dto.NoticeDto;
 import com.yeoro.domain.notice.model.service.NoticeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -42,6 +46,7 @@ public class NoticeController {
     }
 
     @Operation(summary = "공지사항 조회", description = "공지사항 ID를 입력받아 조회해 반환한다.")
+    @Parameter(name = "id", description = "공지사항 아이디", example = "2")
     @GetMapping("/{id}")
     public ResponseEntity<NoticeDto> getNoticeById(@PathVariable Long id) {
         try {
@@ -57,7 +62,22 @@ public class NoticeController {
         }
     }
 
-    @Operation(summary = "공지사항 추가", description = "공지사항을 입력받아 추가한다.")
+    @Operation(
+            summary = "공지사항 추가",
+            description = "공지사항을 입력받아 추가한다.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = NoticeDto.class),
+                            examples = @ExampleObject(
+                                    name = "공지사항 예제",
+                                    summary = "공지사항 예제 입력",
+                                    description = "이 예제는 공지사항 추가를 위한 예제입니다.",
+                                    value = "{\"title\": \"공지사항 제목\", \"context\": \"공지사항 내용\", \"important\": true}"
+                            )
+                    )
+            )
+    )
     @PostMapping
     public ResponseEntity<Void> addNotice(@Valid @RequestBody NoticeDto noticeDto) {
         try {
@@ -73,10 +93,27 @@ public class NoticeController {
         }
     }
 
-    @Operation(summary = "공지사항 수정", description = "공지사항을 입력받아 수정한다.")
+    @Operation(
+            summary = "공지사항 수정",
+            description = "공지사항을 입력받아 수정한다.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = NoticeDto.class),
+                            examples = @ExampleObject(
+                                    name = "공지사항 예제",
+                                    summary = "공지사항 예제 입력",
+                                    description = "이 예제는 공지사항 수정을 위한 예제입니다.",
+                                    value = "{\"title\": \"공지사항 제목 수정\", \"context\": \"공지사항 내용 수정\", \"important\": true}"
+                            )
+                    )
+            )
+    )
+    @Parameter(name = "id", description = "공지사항 아이디", example = "2")
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updateNotice(@PathVariable Long id, @Valid @RequestBody NoticeDto noticeDto) {
         try {
+            noticeDto.setId(id);
             int isOk = noticeService.modifyNotice(noticeDto);
             if (isOk == 1) {
                 return ResponseEntity.ok().build();
@@ -90,6 +127,7 @@ public class NoticeController {
     }
 
     @Operation(summary = "공지사항 삭제", description = "공지사항 ID를 입력받아 삭제한다.")
+    @Parameter(name = "id", description = "공지사항 아이디", example = "2")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteNotice(@PathVariable Long id) {
         try {
